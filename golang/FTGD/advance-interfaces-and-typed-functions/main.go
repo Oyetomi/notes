a00877f501b9aff9a17ad81757875c5291bef2ed
Oyetomi <oyetomiabiodun@yahoo.com>
2023-05-24 10:02:37 +0100

    golang

---
+++ golang/FTGD/advance-interfaces-and-typed-functions/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type Putter interface {
+	Put(id int, val any) error
+	fmt.Stringer
+}
+
+type Storage interface {
+	Putter
+	Get(id int) (any, error)
+}
+type simplePutter struct{}
+
+func (s *simplePutter) Put(id int, val any) error {
+	return nil
+}
+
+func (s *simplePutter) String() string {
+	return ""
+}
+
+type FooStorage struct{}
+
+func (s *FooStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *FooStorage) Put(id int, val any) error {
+	return nil
+}
+
+type BarStorage struct{}
+
+func (s *BarStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *BarStorage) Put(id int, val any) error {
+	return nil
+}
+
+type Server struct {
+	Store Storage
+}
+
+func updateValue(id int, val any, p Putter) error {
+	return p.Put(id, val)
+}
+
+func main() {
+	sputter := &simplePutter{}
+	updateValue(1, "foo", sputter)
+}

385f0c1fca788e9c7a106f569770b7b82dca644d
Oyetomi <oyetomiabiodun@yahoo.com>
2023-05-24 14:26:41 +0100

    golang

--- golang/FTGD/concurrency/main.go
+++ golang/FTGD/concurrency/main.go
@@ -6,15 +6,34 @@ import (
 )
 
 func main() {
+	msgChan := make(chan string, 128)
+	msgChan <- "A"
+	msgChan <- "B"
+	msgChan <- "C"
+	close(msgChan)
+
+	for {
+		msg, closed := <-msgChan
+		if !closed {
+			break
+		}
+		fmt.Println("the message ->", msg)
+	}
+
+	// this piece of code is our consumer
+	for msg := range msgChan {
+		fmt.Println(msg)
+	}
+
 	// 1 unbuffered channel
 	// --> unbuffered channel ... a channel in golang always blocks if it's full
 	// 2 buffered channel
-	resultChan := make(chan string)
-	go func() {
-		result := <-resultChan
-		fmt.Println(result)
-	}()
-	resultChan <- "foo"
+	//resultChan := make(chan string)
+	//go func() {
+	//	result := <-resultChan
+	//	fmt.Println(result)
+	//}()
+	//resultChan <- "foo"
 
 }
 

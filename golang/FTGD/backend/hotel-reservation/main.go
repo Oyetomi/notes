e808bc33208475b605c128b040cdf632ff452a0b
Oyetomi <oyetomiabiodun@yahoo.com>
2023-05-26 13:20:16 +0100

    golang

--- golang/FTGD/backend/hotel-reservation/main.go
+++ golang/FTGD/backend/hotel-reservation/main.go
@@ -1,26 +1,44 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"github.com/Oyetomi/hotel-reservation/api"
+	"github.com/Oyetomi/hotel-reservation/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
-	listenAddr := flag.String("listenAddr", ":9000", "The listen address to of the API server")
+	listenAddr := flag.String("listenAddr", ":9000", "The listen address to the API server")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
-	err := app.Listen(*listenAddr)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
+	err = app.Listen(*listenAddr)
 	if err != nil {
 		return
 	}
-}
-
-func handleFoo(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"message": "working just fine"})
 }

5404e5b2d664ac8bad7338d609adc37078d43254
Oyetomi <oyetomiabiodun@yahoo.com>
2023-05-26 22:16:43 +0100

    golang

--- golang/FTGD/backend/hotel-reservation/db/user_store.go
+++ golang/FTGD/backend/hotel-reservation/db/user_store.go
@@ -12,6 +12,8 @@ const userColl = "users"
 
 type UserStore interface {
 	GetUserByID(ctx context.Context, id string) (*types.User, error)
+	GetUsers(context.Context) ([]*types.User, error)
+	InsertUser(ctx context.Context, user *types.User) (*types.User, error)
 }
 
 type MongoUserStore struct {
@@ -25,6 +27,27 @@ func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 		client:     client,
 		collection: client.Database(DB_NAME).Collection(userColl),
 	}
+}
+
+func (s *MongoUserStore) InsertUser(ctx context.Context, user *types.User) (*types.User, error) {
+	res, err := s.collection.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	return user, nil
+}
+
+func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
+	cursor, err := s.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	var users []*types.User
+	if err := cursor.All(ctx, &users); err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 
 func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {

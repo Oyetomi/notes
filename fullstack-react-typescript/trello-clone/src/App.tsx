38014ffbc7bef0cd94c1a77cf4f223238c2a3050
Oyetomi <oyetomiabiodun@yahoo.com>
2023-05-29 15:24:57 +0100

    new item component

--- fullstack-react-typescript/trello-clone/src/App.tsx
+++ fullstack-react-typescript/trello-clone/src/App.tsx
@@ -1,10 +1,15 @@
 import { AppContainer } from "./styles";
 import { Column } from "./Column";
+import { AddNewItem } from "./AddNewItem";
 
 export const App = () => {
   return (
     <AppContainer>
       <Column text="Todo:"></Column>
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={() => console.log("item created")}
+      ></AddNewItem>
     </AppContainer>
   );
 };
--- fullstack-react-typescript/trello-clone/src/Column.tsx
+++ fullstack-react-typescript/trello-clone/src/Column.tsx
@@ -1,5 +1,6 @@
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { Card } from "./Card";
+import { AddNewItem } from "./AddNewItem";
 
 type ColumnProps = {
   text: string;
@@ -11,6 +12,11 @@ export const Column = ({ text }: ColumnProps) => {
       <Card text="Generate app scaffold"></Card>
       <Card text="Learn Typescript"></Card>
       <Card text="Begin to use static type"></Card>
+      <AddNewItem
+        toggleButtonText="+ Add another card"
+        onAdd={() => console.log("item created")}
+        dark
+      ></AddNewItem>
     </ColumnContainer>
   );
 };

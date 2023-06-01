b3be5e0b2a68111696dae0ded1a34ca1d5636e47
Oyetomi <oyetomiabiodun@yahoo.com>
2023-06-01 19:56:15 +0100

    09-promises.problem.ts

--- typescript/total-typescript/typescript-basics/09-promises.problem.ts
+++ typescript/total-typescript/typescript-basics/09-promises.problem.ts
@@ -10,9 +10,13 @@ interface LukeSkywalker {
 }
 
 export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
-  const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
+  const data: LukeSkywalker = await fetch(
+    "https://swapi.dev/api/people/1"
+  ).then((res) => {
     return res.json();
   });
 
-  return data;
+  return data as LukeSkywalker;
 };
+
+const luke = fetchLukeSkywalker();

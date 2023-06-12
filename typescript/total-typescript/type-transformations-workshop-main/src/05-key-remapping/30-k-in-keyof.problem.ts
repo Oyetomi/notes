c0663c80d4a138831e1ff4fd928c8a45130580a1
Oyetomi <oyetomiabiodun@yahoo.com>
2023-06-12 17:55:12 +0100

    30-k-in-keyof.problem.ts

---
+++ typescript/total-typescript/type-transformations-workshop-main/src/05-key-remapping/30-k-in-keyof.problem.ts
@@ -0,0 +1,24 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+interface Attributes {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+type AttributeGetters = {
+  [k in keyof Attributes]: () => Attributes[k];
+};
+
+type tests = [
+  Expect<
+    Equal<
+      AttributeGetters,
+      {
+        firstName: () => string;
+        lastName: () => string;
+        age: () => number;
+      }
+    >
+  >
+];

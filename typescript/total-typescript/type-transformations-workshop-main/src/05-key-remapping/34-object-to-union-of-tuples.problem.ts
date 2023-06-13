572163030eb36d61ea86325d81cbf4e1e8f99b65
Oyetomi <oyetomiabiodun@yahoo.com>
2023-06-13 20:19:13 +0100

    34-object-to-union-of-tuples.problem.ts

---
+++ typescript/total-typescript/type-transformations-workshop-main/src/05-key-remapping/34-object-to-union-of-tuples.problem.ts
@@ -0,0 +1,22 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+interface Values {
+  email: string;
+  firstName: string;
+  lastName: string;
+}
+
+type VOV = Values[keyof Values];
+
+type ValuesAsUnionOfTuples = {
+  [V in keyof Values]: [V, Values[V]];
+}[keyof Values];
+
+type tests = [
+  Expect<
+    Equal<
+      ValuesAsUnionOfTuples,
+      ["email", string] | ["firstName", string] | ["lastName", string]
+    >
+  >
+];

9cdf54488725bfb5de2109c3b40df22680564f1c
Oyetomi <oyetomiabiodun@yahoo.com>
2023-06-19 14:37:08 +0100

    props

--- react-js/hacker-stories/src/App.tsx
+++ react-js/hacker-stories/src/App.tsx
@@ -1,36 +1,49 @@
+import React from "react";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+type storiesProp = {
+  title: string;
+  url: string;
+  author: string;
+  num_comments: number;
+  points: number;
+  objectID: number;
+};
 
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 };
 
-const List = () => {
+type ListProps = {
+  list: storiesProp[];
+};
+
+const List: React.FC<ListProps> = ({ list }) => {
   return (
     <ul>
       {list.map((item) => (

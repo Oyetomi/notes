b05ede18ea4b75621e83161be2f79830229e61cf
Oyetomi <oyetomiabiodun@yahoo.com>
2023-06-23 17:57:58 +0100

    Relationship Between TypeScript and JavaScript

---
+++ typescript/effective-typescript/typescript-javascript.js
@@ -0,0 +1,9 @@
+"use strict";
+let city = "new york city";
+console.log(city.toUpperCase());
+const states = [{ name: "Alabama", capital: "Montgomery" }];
+[{ name: "Alaska", capital: "Juneau" }];
+[{ name: "Arizona", capital: "Phoenix" }];
+for (const state of states) {
+    console.log(state.capital);
+}
---
+++ typescript/effective-typescript/typescript-javascript.ts
@@ -0,0 +1,15 @@
+let city = "new york city";
+console.log(city.toUpperCase());
+
+interface State {
+  name: string;
+  capital: string;
+}
+
+const states: State[] = [{ name: "Alabama", capital: "Montgomery" }];
+[{ name: "Alaska", capital: "Juneau" }];
+[{ name: "Arizona", capital: "Phoenix" }];
+
+for (const state of states) {
+  console.log(state.capital);
+}

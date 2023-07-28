0faca64379a31ef20f42713c14996b1b082e0187
Oyetomi <oyetomiabiodun@yahoo.com>
2023-07-28 23:16:33 +0100

    array project

--- react-js/react-simplified/react-js/React-Simplified-Beginner-Projects-main/20-array-component/src/App.jsx
+++ react-js/react-simplified/react-js/React-Simplified-Beginner-Projects-main/20-array-component/src/App.jsx
@@ -1,89 +1,83 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const INITIAL_VALUE = ["A", "B", "C"]
+const INITIAL_VALUE = ["A", "B", "C"];
 
 function App() {
-  const [array, setArray] = useState(INITIAL_VALUE)
-  const [value, setValue] = useState("")
+  const [array, setArray] = useState(INITIAL_VALUE);
+  const [value, setValue] = useState("");
+  const removeFirstElement = () => {
+    setArray((currentArray) => {
+      return currentArray.slice(1);
+    });
+  };
 
-  function removeFirstElement() {
-    setArray(currentArray => {
-      return currentArray.slice(1)
-    })
-  }
+  const removeSpecificLetter = (letter) => {
+    setArray((currentArray) => {
+      return currentArray.filter((element) => element !== letter);
+    });
+  };
 
-  function removeSpecificLetter(letter) {
-    setArray(currentArray => {
-      return currentArray.filter(element => element !== letter)
-    })
-  }
+  const addLetterToStart = (letter) => {
+    setArray((currentArray) => {
+      return [letter, ...currentArray];
+    });
+  };
 
-  function addLetterToStart(letter) {
-    setArray(currentArray => {
-      return [letter, ...currentArray]
-    })
-  }
+  const addLetterToEnd = (letter) => {
+    setArray((currentArray) => {
+      return [...currentArray, letter];
+    });
+  };
 
-  function addLetterToEnd(letter) {
-    setArray(currentArray => {
-      return [...currentArray, letter]
-    })
-  }
+  const clearArray = () => {
+    setArray((currentArray) => {
+      return [];
+    });
+  };
 
-  function clear() {
-    setArray([])
-  }
+  const resetArray = () => {
+    setArray(INITIAL_VALUE);
+  };
 
-  function reset() {
-    setArray(INITIAL_VALUE)
-  }
+  const updateAToH = () => {
+    setArray((currentArray) => {
+      return currentArray.map((element) => {
+        if (element === "A") {
+          return "H";
+        }
+        return element;
+      });
+    });
+  };
 
-  function updateAToH() {
-    setArray(currentArray => {
-      return currentArray.map(element => {
-        if (element === "A") return "H"
-        return element
-      })
-    })
-  }
-
-  function addLetterAtIndex(letter, index) {
-    setArray(currentArray => {
+  const addLetterAtIndex = (letter, index) => {
+    setArray((currentArray) => {
       return [
         ...currentArray.slice(0, index),
         letter,
         ...currentArray.slice(index),
-      ]
-    })
-  }
+      ];
+    });
+  };
 
   return (
     <div>
       <button onClick={removeFirstElement}>Remove First Element</button>
-      <br />
       <button onClick={() => removeSpecificLetter("B")}>Remove All B's</button>
-      <br />
-      <button onClick={() => addLetterToStart("B")}>Add To Start</button>
-      <br />
-      <button onClick={() => addLetterToEnd("Z")}>Add To End</button>
-      <br />
-      <button onClick={clear}>Clear</button>
-      <br />
-      <button onClick={reset}>Reset</button>
-      <br />
-      <button onClick={updateAToH}>Update A To H</button>
-      <br />
-      <button onClick={() => addLetterAtIndex("C", 2)}>Add C At 2</button>
-      <br />
-      <input value={value} onChange={e => setValue(e.target.value)} />
-      <br />
+      <button onClick={() => addLetterToStart("B")}>Add to start</button>
+      <button onClick={() => addLetterToEnd("Z")}>Add to end</button>
+      <button onClick={clearArray}>Clear the array</button>
+      <button onClick={resetArray}>Reset the array</button>
+      <button onClick={updateAToH}>update A to H</button>
+      <button onClick={() => addLetterAtIndex("c", 2)}>Add C at 2</button>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
       <button onClick={() => addLetterToStart(value)}>
         Add Value To Array
       </button>
       <br />
       {array.join(", ")}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

6875e61c4564b5b4e8117025d60425c2bcbfb6ee
Oyetomi <oyetomiabiodun@yahoo.com>
2023-09-04 10:30:26 +0100

    mocking esm modules

---
+++ nodejs-design/modifying-modules/mock-read-file.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+
+const originalReadFile = fs.readFile;
+let mockedResponse = null;
+
+function mockReadFile(path, cb) {
+  setImmediate(() => {
+    cb(null, mockedResponse);
+  });
+}
+
+export function mockEnable(respondWith) {
+  mockedResponse = respondWith;
+  fs.readFile = mockReadFile;
+}
+
+export function mockDisable() {
+  fs.readFile = originalReadFile;
+}
